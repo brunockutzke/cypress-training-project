27e9c5d903871c5429d32fe67849b4ede743583f
Bruno Kutzke <bruno.kutzke@wisereducacao.com>
2022-03-07 21:29:04 -0300

    Create cypress basic project structure

---
+++ cypress/integration/store.test.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Store Home Page - Scenarios', () => {
+    it('Visit Store Home Page', () => {
+        cy.visit(Cypress.env('BASE_URL'))
+    })
+
+    it('Perform a simple search', () => {
+        cy.visit(Cypress.env('BASE_URL'))
+        cy.get('#search_query_top').type('shirt')
+        cy.get('.ac_results')
+        .should('be.visible')
+        .children()
+        .click()
+    })
+
+
+})
